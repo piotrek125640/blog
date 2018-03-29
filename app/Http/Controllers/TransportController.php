7889db475a3db264c32b5a767aa55c41ab91a358
Piotr Szydlik <piotr-szydlik@o2.pl>
2018-03-29 16:18:07 +0200

    Widok: Transport, Diagnostyka, Warsztat + aktualizacja web.php

---
+++ app/Http/Controllers/TransportController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransportController extends Controller
+{
+    public function index(){
+		return view('transport.index');
+		}
