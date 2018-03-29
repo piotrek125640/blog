1caec06edc904c8cd5986c9f84e11efa0dd2b66f
Lladdmi <s.kaszkiel@gmail.com>
2018-03-29 16:47:05 +0200

    body update

--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -3,54 +3,86 @@
         <!-- Three columns of text below the carousel -->
         <div class="row">
           <div class="col-lg-4">
-            <div class="card-group">
               <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-            <!-- <h2></h2>
-            <i class="fas fa-truck-loading"></i>
-            <span id="myTargetElement"></span>
-            <script type="text/javascript">
-            var options = {
-              useEasing: true,
-              useGrouping: true,
-              separator: ',',
-              decimal: '.',
-            };
-            var demo = new CountUp('myTargetElement', 0, 4391, 0, 2.5, options);
-            if (!demo.error) {
-              demo.start();
-            } else {
-              console.error(demo.error);
-            }
+                  <h5 class="card-title"><i class="far fa-user fa-3x"></i></h5>
+                  <p class="card-text display-4">
+                    <span id="trucks"></span>
+                    <script type="text/javascript">
+                    var options = {
+                      useEasing: true,
+                      useGrouping: true,
+                      separator: ',',
+                      decimal: '.',
+                    };
+                    var demo = new CountUp('trucks', 0, 10, 0, 2.5, options);
+                    if (!demo.error) {
+                      demo.start();
+                    } else {
+                      console.error(demo.error);
+                    }
 
-            </script>
-          </div> -->
-          <div class="col-lg-4">
-            <div class="card-group">
-              <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </script>
+                  </p>
                 </div>
+                <div class="card-footer">
+                  <h5>text</h5>
+                </div>
+              </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title"><i class="fas fa-cube fa-3x"></i></h5>
+                <p class="card-text">
+                  <span id="load" class="display-4"></span>
+                  <script type="text/javascript">
+                  var options = {
+                    useEasing: true,
+                    useGrouping: true,
+                    separator: ',',
+                    decimal: '.',
+                  };
+                  var demo = new CountUp('load', 0, 20, 0, 2.5, options);
+                  if (!demo.error) {
+                    demo.start();
+                  } else {
+                    console.error(demo.error);
+                  }
+
+                  </script>
+                </p>
+              </div>
+              <div class="card-footer">
+                <h5>text</h5>
               </div>
             </div>
           </div>
-          <div class="col-lg-4">>
-            <div class="card-group">
-              <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title"><i class="fas fa-truck-loading fa-3x"></i></h5>
+                <p class="card-text display-4">
+                  <span id="emplo"></span>
+                  <script type="text/javascript">
+                  var options = {
+                    useEasing: true,
+                    useGrouping: true,
+                    separator: ',',
+                    decimal: '.',
+                  };
+                  var demo = new CountUp('emplo', 0, 30, 0, 2.5, options);
+                  if (!demo.error) {
+                    demo.start();
+                  } else {
+                    console.error(demo.error);
+                  }
+
+                  </script>
+                </p>
+              </div>
+              <div class="card-footer">
+                <h5>Ciężarówek</h5>
               </div>
             </div>
           </div>
