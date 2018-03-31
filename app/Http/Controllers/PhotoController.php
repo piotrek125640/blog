dc99edb989a0d7267827ffe656d12ee0f01babf1
Lladdmi <s.kaszkiel@gmail.com>
2018-03-31 12:55:22 +0200

    fix migration

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -7,12 +7,8 @@ use App\Photo;
 
 class PhotoController extends Controller
 {
-<<<<<<< HEAD
-
-=======
     public function index(){
       $photo = Photo::latest()->get();
   return view('diagnostyka.index',compact('photo'));
   }
->>>>>>> 250b4e85174b1510c7301d924df656e14c170c5a
 }
