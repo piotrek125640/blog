5351e46fc84effc4fcc58a37c2643286a23dd54d
Piotr Szydlik <piotr-szydlik@o2.pl>
2018-03-31 18:45:13 +0200

    nic ważnego

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,4 +1,3 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
  <link rel="stylesheet"  type="text/css"href="<?php echo asset('css/style.css')?>">
- <link rel="stylesheet"  type="text/css"href="<?php echo asset('css/style1.css')?>">
