5c02ce43048245b4d70b5d052e795c3308579413
Lladdmi <s.kaszkiel@gmail.com>
2018-04-14 17:21:19 +0200

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
     <footer id="myFooter">
-        <div class="container">
+        <div class="container mx-auto">
             <div class="row">
-              <div class="col-sm-6">
+              <div class="col-sm-5">
                 <ul>
                   <li><img src="{{asset('gallery/henderson2.png')}}" style="width: 200px; padding-top:15px;"> </li>
                   <li style="margin-top: -10px;"><a href="#"></a><small>Okręgowa Stacja Kontroli Pojazdów</small></li>
@@ -12,7 +12,7 @@
                   </div>
                 </ul>
               </div>
-              <div class="col-sm-3">
+              <div class="col-sm-2">
                 <h5>Informacje</h5>
                   <ul>
                     <li><a href="#">O nas</a></li>
@@ -20,17 +20,16 @@
                     <li><a href="#">FAQ</a></li>
                 </ul>
               </div>
-              <div class="col-sm-3">
+              <div class="col-sm-5">
                 <h5>Skontaktuj się z nami</h5>
                   <ul>
-                    <li><a href="#">Wyszków 07-221</br>ul. Towarowa 10</a></li>
+                    <li><a href="#">Wyszków 07-221 ul. Towarowa 10</a></li>
                     <li><a href="#">Telefon: 500 064 908</a></li>
                     <li><a href="#">Email: stacjam10@wp.pl</a></li>
                 </ul>
               </div>
             </div>
         </div>
-
         <div class="footer-copyright">
             <p>Copyright © 2018 Hederson24.pl</p>
         </div>
