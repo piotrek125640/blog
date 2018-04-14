cd0bde8c61ecfc181121fa08cac05ff3cebdf413
Lladdmi <s.kaszkiel@gmail.com>
2018-04-14 16:57:28 +0200

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,56 +1,37 @@
     <footer id="myFooter">
         <div class="container">
             <div class="row">
-                <div class="col-sm-3">
-                    <h5>Get started</h5>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Sign up</a></li>
-                        <li><a href="#">Downloads</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>About us</h5>
-                    <ul>
-                        <li><a href="#">Company Information</a></li>
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Reviews</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Support</h5>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Help desk</a></li>
-                        <li><a href="#">Forums</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3">
-                    <h5>Legal</h5>
-                    <ul>
-                        <li><a href="#">Terms of Service</a></li>
-                        <li><a href="#">Terms of Use</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="social-networks">
-          <div class="container">
-            <div class="row justify-content-md-center text-center">
-              <div class="col col-lg-1">
-                <a href="#" class="twitter"><i class="fab fa-twitter fa-3x"></i></a>
+              <div class="col-sm-6">
+                <ul>
+                  <li><img src="{{asset('gallery/henderson2.png')}}" style="width: 200px; padding-top:15px;"> </li>
+                  <li style="margin-top: -10px;"><a href="#"></a><small>Okręgowa Stacja Kontroli Pojazdów</small></li>
+                  <div style="line-height: 1.2; display: inline-flex; padding-top: 20px;"><a href="https://www.facebook.com/HendersonDiagnostykaSamochodowaWyszkow/">
+                    <div style="padding-right: 10px;"><i class="fab fa-facebook-f fa-2x"></i></div>
+                    <div><a style="color: #e2e2e2;">@HendersonDiagnostyka</br>
+                     &nbsp; &nbsp;SamochodowaWyszkow</a></div>
+                  </div>
+                </ul>
               </div>
-              <div class="col col-lg-1">
-                <a href="#" class="facebook"><i class="fab fa-facebook fa-3x"></i></a>
+              <div class="col-sm-3">
+                <h5>Informacje</h5>
+                  <ul>
+                    <li><a href="#">O nas</a></li>
+                    <li><a href="#">Kontakt</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
               </div>
-              <div class="col col-lg-1">
-                <a href="#" class="google"><i class="fab fa-google-plus-g fa-3x"></i></a>
+              <div class="col-sm-3">
+                <h5>Skontaktuj się z nami</h5>
+                  <ul>
+                    <li><a href="#">Wyszków 02-221</br>ul. Towarowa 10</a></li>
+                    <li><a href="#">Telefon: 500 064 908</a></li>
+                    <li><a href="#">Email: stacjam10@wp.pl</a></li>
+                </ul>
               </div>
             </div>
-          </div>
         </div>
+
         <div class="footer-copyright">
-            <p>© 2016 Copyright Text </p>
+            <p>Copyright © 2018 Hederson24.pl</p>
         </div>
     </footer>
